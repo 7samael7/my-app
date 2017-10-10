7d3fca3ce0155c41453b6d082bd77708767e452c
Unknown <sebastianhruby@gmail.com>
2017-10-10 10:05:27 +0200

    adding annotate and validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: tests
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  questions  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Test < ApplicationRecord
+  #simple validation syntax (not so great)
+  validates_presence_of :title
+  validates_numericality_of :titel
+
+  validates :title, presence: true
 end
