b1b052aa6e934d3d4b3ecc255fa16a25ef5d422a
Unknown <sebastianhruby@gmail.com>
2017-10-24 11:57:19 +0200

    enumerations

--- app/models/question.rb
+++ app/models/question.rb
@@ -19,6 +19,11 @@
 class Question < ApplicationRecord
   has_many :question_assignments
   has_many :tests, through: :question_assignments
+  has_many :answers
 
   belongs_to :topic
+
+  extend Enumerize
+
+  enumerize :difficulty, in: { normal: 1, nightmare: 2, hell: 3 }, default: :normal , scope: true, predicates: true
 end
