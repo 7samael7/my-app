55b9155adb7e036407bb56617aab989f9773670f
Unknown <sebastianhruby@gmail.com>
2017-10-24 11:41:52 +0200

    adding questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024093329) do
+ActiveRecord::Schema.define(version: 20171024094036) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "topic_id"
+    t.string "title"
+    t.integer "score"
+    t.string "hint"
+    t.text "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_questions_on_topic_id"
   end
 
   create_table "study_programs", force: :cascade do |t|
@@ -43,7 +54,6 @@ ActiveRecord::Schema.define(version: 20171024093329) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title"
-    t.text "questions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
