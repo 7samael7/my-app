b9d9eb71d45b2fcfe9e051f03b8b66c9c379f765
Unknown <sebastianhruby@gmail.com>
2017-10-24 11:32:49 +0200

    creating tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017090445) do
+ActiveRecord::Schema.define(version: 20171024092924) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "study_programs", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
